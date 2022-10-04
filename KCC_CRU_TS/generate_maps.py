3d81e865f2048554dc2ddbf8b95dff82bcfe4362
Antoine DEMANY <nolmetarxv@gmail.com>
2022-10-04 12:06:23 +0200

    Gen maps: Added titles, legends, all types of map

--- KCC_CRU_TS/generate_maps.py
+++ KCC_CRU_TS/generate_maps.py
@@ -1,6 +1,7 @@
 #!python3
 import os
 import random
+import datetime
 import cartopy.crs as ccrs
 import cartopy.feature as c_feature
 import matplotlib.pyplot as plt
@@ -14,6 +15,33 @@ class GenerateMaps:
     def __init__(self):
         print("Generate Maps: init")
         self.ko = Koppen()
+        self.map_name = {
+            1: "Yearly Köppen climate classification map",
+            2: "Yearly minimum of monthly temperature (degrees Celsius)",
+            3: "Yearly mean of monthly temperature (degrees Celsius)",
+            4: "Yearly maximum of monthly temperature (degrees Celsius)",
+            5: "Yearly minimum of monthly precipitation (millimetres per month)",
+            6: "Yearly mean of monthly precipitation (millimetres per month)",
+            7: "Yearly maximum of monthly precipitation (millimetres per month)",
+            8: "Yearly sum precipitation (millimetres per month)",
+            9: "Yearly maximum of monthly relative humidity (percentage)",
+            10: "Yearly minimum of monthly wet-bulb temperature (degrees Celsius)",
+            11: "Yearly mean of monthly relative humidity (percentage)",
+            12: "Yearly mean of monthly wet-bulb temperature (degrees Celsius)",
+            13: "Yearly minimum of monthly relative humidity (percentage)",
+            14: "Yearly maximum of monthly wet-bulb temperature (degrees Celsius)",
+            15: "Yearly minimum of monthly cloud cover (percentage)",
+            16: "Yearly mean of monthly cloud cover (percentage)",
+            17: "Yearly maximum of monthly cloud cover (percentage)",
+            18: "Yearly minimum of monthly wet days (days)",
+            19: "Yearly mean of monthly wet days (days)",
+            20: "Yearly maximum of monthly wet days (days)",
+            21: "Yearly sum of monthly wet days (days)",
+            22: "Yearly minimum of monthly frost days (days)",
+            23: "Yearly mean of monthly frost days (days)",
+            24: "Yearly maximum of monthly frost days (days)",
+            25: "Yearly sum of monthly frost days (days)"
+        }
 
     def generate_map_climates(self, out_dir, io, name, name_type, year, props: list):
         if not os.path.exists(out_dir):
@@ -38,16 +66,21 @@ class GenerateMaps:
             patches.append(rectangle)
         p = PatchCollection(patches, alpha=1, facecolors=colors)
         chart.add_collection(p)
-
         chart.add_feature(c_feature.BORDERS, linestyle='-', linewidth=0.5)
-        chart.set_title(str(year) + " " + name)
+
+        now = datetime.datetime.now()
+        text = "Legend: See 'Köppen climate classification' on wikipedia, white (no data)"
+        text = text + " \nMade by climatle.earth (" + now.strftime("%Y-%m-%d") + ") with " \
+                      "data from the Climatic Research Unit (University of East Anglia) and NCAS."
+        plt.text(-175, -85, text, fontsize=10, backgroundcolor='white')
+
+        chart.set_title(str(year) + " " + self.map_name[name_type], fontsize=16)
         plt.savefig(path, bbox_inches='tight')
         plt.close(fig)
         del fig
         print("Generate Maps: finished map {} generation".format(path))
 
-    @staticmethod
-    def generate_map_scale(out_dir, name, name_type, year, props: list, map_type, min_val, max_val):
+    def generate_map_scale(self, out_dir, name, name_type, year, props: list, map_type, min_val, max_val):
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
         path = out_dir + str(year) + "-" + str(name_type) + "-" + name + ".jpg"
@@ -110,9 +143,28 @@ class GenerateMaps:
             patches.append(rectangle)
         p = PatchCollection(patches, alpha=1, facecolors=colors)
         chart.add_collection(p)
-
         chart.add_feature(c_feature.BORDERS, linestyle='-', linewidth=0.5)
-        chart.set_title(str(year) + " " + name)
+
+        text = ""
+        if map_type == "rb":
+            text = "Legend (gradient): blue (" + str(min_val) + \
+                   ") to white (0), white (0) to red (" + str(max_val) + "), gray (no data)"
+        elif map_type == "r":
+            text = "Legend (gradient): white (" + str(min_val) + \
+                   ") to red (" + str(max_val) + "), gray (no data)"
+        elif map_type == "b":
+            text = "Legend (gradient): white (" + str(min_val) + \
+                   ") to blue (" + str(max_val) + "), gray (no data)"
+        elif map_type == "b2":
+            text = "Legend (gradient): white (" + str(min_val) + \
+                   ") to blue (" + str(max_val) + "), gray (no data)"
+        now = datetime.datetime.now()
+        text = text + " \nMade by climatle.earth (" + now.strftime("%Y-%m-%d") + ") with " \
+                      "data from the Climatic Research Unit (University of East Anglia) and NCAS."
+
+        plt.text(-175, -85, text, fontsize=10, backgroundcolor='white')
+
+        chart.set_title(str(year) + " " + self.map_name[name_type], fontsize=16)
         plt.savefig(path, bbox_inches='tight')
         plt.close(fig)
         del fig
